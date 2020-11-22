89cd1d4fa0d2ae8ff8c13ec0be4246c3853963ba
Balázs Tóth <gawnoris@gmail.com>
2020-11-22 15:30:51 +0100

    Added a ship type property for the Ship class,a place_boats method which now can place the first 2 types down.

--- torpedo_project/GameObjects/Ship.cs
+++ torpedo_project/GameObjects/Ship.cs
@@ -8,21 +8,146 @@ namespace torpedo_project.GameObjects
 {
     class Ship
     {
-        private char x_start { get; set; }
-        private char y_start { get; set; }
-        private char x_end { get; set; }
-        private char y_end { get; set; }
+        private string x_start { get; set; }
+        private int y_start { get; set; }
+        private string x_end { get; set; }
+        private int y_end { get; set; }
+        public string shipType { get; set; }
 
-        public Ship(char x_start, char y_start, char x_end, char y_end) {
+        enum ALPHABETS {
+                A = 'A',
+                B = 'B',
+                C='C',
+                D='D',
+                E='E',
+                F='F',
+                G='G',
+                H='H',
+                I='I',
+                J='J'
+        }
+
+        public Ship(string x_start, int y_start, string x_end, int y_end,string shipType) {
             this.x_start = x_start;
             this.y_start = y_start;
             this.x_end = x_end;
             this.y_end = y_end;
+            this.shipType = shipType;
         }
 
-        public char[,] getCoords() {
-            char[,] coords = new char[,] { {x_start, y_start }, { x_end, y_end } };
-            return coords;
+        public string[,] getCoords() {
+            string[,] coords2x;
+            string[,] coords3x;
+            string[,] coords4x;
+            string[,] coords5x;
+            switch (shipType) {
+                case "Patrol Boat":
+                coords2x = new string[,] { { x_start, y_start.ToString() }, { x_end, y_end.ToString() } };
+                return coords2x;
+                case "Submarine":
+                    if (x_start.Equals(x_end))
+                    {
+                        if (y_end - y_start == 2)
+                        {
+                            coords3x = new string[,] { { x_start, y_start.ToString() },
+                                                       { x_end, (y_end - 1).ToString() }, 
+                                                       { x_end, y_end.ToString() } };
+                            return coords3x;
+                        }
+                        else return null;
+                    }
+                    else if (y_start.Equals(y_end))
+                    {
+                        if (check_for_alphabetic_order(x_start, x_end,shipType))
+                        {
+                            coords3x = new string[,] { { x_start, y_start.ToString() },
+                                                       { GetMiddlecoord(x_start).ToString(), y_end.ToString() },
+                                                       { x_end, y_end.ToString() } };
+                            return coords3x;
+                        }
+                        else return null;
+                    }
+                    else return null;
+                case "Destroyer":
+                    coords3x = new string[,] { { x_start, y_start.ToString() }, { x_end, y_end.ToString() } };
+                    return coords3x;
+                case "Battleship":
+                    coords4x = new string[,] { { x_start, y_start.ToString() }, { x_end, y_end.ToString() } };
+                    return coords4x;
+                case "Carrier":
+                    coords5x = new string[,] { { x_start, y_start.ToString() }, { x_end, y_end.ToString() } };
+                    return coords5x;
+                default:
+                    return null;
+            }
+        }
+
+
+        private string GetMiddlecoord(string x_start) {
+            if (x_start.Equals((ALPHABETS.A))) return ALPHABETS.B.ToString();
+            else if (x_start.Equals(ALPHABETS.B.ToString())) return ALPHABETS.C.ToString();
+            else if (x_start.Equals(ALPHABETS.C.ToString())) return ALPHABETS.D.ToString();
+            else if (x_start.Equals(ALPHABETS.D.ToString())) return ALPHABETS.E.ToString();
+            else if (x_start.Equals(ALPHABETS.E.ToString())) return ALPHABETS.F.ToString();
+            else if (x_start.Equals(ALPHABETS.F.ToString())) return ALPHABETS.G.ToString();
+            else if (x_start.Equals(ALPHABETS.G.ToString())) return ALPHABETS.H.ToString();
+            else if (x_start.Equals(ALPHABETS.H.ToString())) return ALPHABETS.I.ToString();
+            else if (x_start.Equals(ALPHABETS.I.ToString())) return ALPHABETS.J.ToString();
+            else return ALPHABETS.A.ToString();
+        }
+
+        private bool check_for_alphabetic_order(string start, string end, string shiptype)
+        {
+            if (shiptype.Equals("Submarine"))
+            {
+                if (start.Equals("A") && end.Equals("C")) return true;
+                else if (start.Equals("B") && end.Equals("D")) return true;
+                else if (start.Equals("C") && end.Equals("E")) return true;
+                else if (start.Equals("D") && end.Equals("F")) return true;
+                else if (start.Equals("E") && end.Equals("G")) return true;
+                else if (start.Equals("F") && end.Equals("H")) return true;
+                else if (start.Equals("G") && end.Equals("I")) return true;
+                else if (start.Equals("H") && end.Equals("J")) return true;
+                else return false;
+            }
+            //TODO make the checking for all the ship types
+            else if (shiptype.Equals("Destroyer"))
+            {
+                if (start.Equals("A") && end.Equals("C")) return true;
+                else if (start.Equals("B") && end.Equals("D")) return true;
+                else if (start.Equals("C") && end.Equals("E")) return true;
+                else if (start.Equals("D") && end.Equals("F")) return true;
+                else if (start.Equals("E") && end.Equals("G")) return true;
+                else if (start.Equals("F") && end.Equals("H")) return true;
+                else if (start.Equals("G") && end.Equals("I")) return true;
+                else if (start.Equals("H") && end.Equals("J")) return true;
+                else return false;
+            }
+            else if (shiptype.Equals("Battleship"))
+            {
+                if (start.Equals("A") && end.Equals("C")) return true;
+                else if (start.Equals("B") && end.Equals("D")) return true;
+                else if (start.Equals("C") && end.Equals("E")) return true;
+                else if (start.Equals("D") && end.Equals("F")) return true;
+                else if (start.Equals("E") && end.Equals("G")) return true;
+                else if (start.Equals("F") && end.Equals("H")) return true;
+                else if (start.Equals("G") && end.Equals("I")) return true;
+                else if (start.Equals("H") && end.Equals("J")) return true;
+                else return false;
+            }
+            else if (shiptype.Equals("Carrier"))
+            {
+                if (start.Equals("A") && end.Equals("C")) return true;
+                else if (start.Equals("B") && end.Equals("D")) return true;
+                else if (start.Equals("C") && end.Equals("E")) return true;
+                else if (start.Equals("D") && end.Equals("F")) return true;
+                else if (start.Equals("E") && end.Equals("G")) return true;
+                else if (start.Equals("F") && end.Equals("H")) return true;
+                else if (start.Equals("G") && end.Equals("I")) return true;
+                else if (start.Equals("H") && end.Equals("J")) return true;
+                else return false;
+            }
+            else return false;
         }
     }
 }
--- torpedo_project/MainWindow.xaml.cs
+++ torpedo_project/MainWindow.xaml.cs
@@ -30,28 +30,17 @@ namespace torpedo_project
         {
             GameObjects.Player player1 = new GameObjects.Player("test_player");
             player_name_test_label.Content = player1.PlayerName;
-            player1.fillUpRemainingShips(new GameObjects.Ship('A', '1','A','2'));
-            player1.fillUpRemainingShips(new GameObjects.Ship('A', '5', 'A', '5'));
-            player1.fillUpRemainingShips(new GameObjects.Ship('C', '1', 'D', '1'));
+            player1.fillUpRemainingShips(new GameObjects.Ship("A", 1,"A",2, "Patrol Boat"));
+            player1.fillUpRemainingShips(new GameObjects.Ship("A", 5, "A", 7, "Submarine"));
+            player1.fillUpRemainingShips(new GameObjects.Ship("C", 1, "D", 1, "Patrol Boat"));
+            player1.fillUpRemainingShips(new GameObjects.Ship("B", 5, "D", 5, "Submarine"));
+            player1.fillUpRemainingShips(new GameObjects.Ship("E", 3, "G", 3, "Submarine"));
+            player1.fillUpRemainingShips(new GameObjects.Ship("I", 1, "I", 3, "Submarine"));
 
-            foreach (GameObjects.Ship ship in player1.RemainingShips)
-            {
-                if (AreCoordsSame("" + ship.getCoords()[0, 0] + "" + ship.getCoords()[0, 1],
-                                                 "" + ship.getCoords()[1, 0] + "" + ship.getCoords()[1, 1]))
-                {
-                    Button toDrawInto = (Button)PlayerShipTable.FindName(ship.getCoords()[0, 0] + "" + ship.getCoords()[0, 1]);
-                    toDrawInto.Content = "O";
-                }
-                else {
-                    Button toDrawStart = (Button)PlayerShipTable.FindName(ship.getCoords()[0, 0] + "" + ship.getCoords()[0, 1]);
-                    toDrawStart.Content = "O";
-                    Button toDrawEnd = (Button)PlayerShipTable.FindName(ship.getCoords()[1, 0] + "" + ship.getCoords()[1, 1]);
-                    toDrawEnd.Content = "O";
-                }
-            }
-            Console.WriteLine(player1.RemainingShips);
+            place_boats(player1);
         }
 
+        //the function can still be useful for checking if a player "hits" a ship
         private bool AreCoordsSame(string coordsStart,string coordsEnd) {
             if (coordsStart.Equals(coordsEnd))
             {
@@ -60,5 +49,36 @@ namespace torpedo_project
             else return false;
             
         }
+
+        private void place_boats(GameObjects.Player player) {
+            foreach (GameObjects.Ship ship in player.RemainingShips)
+            {
+                if (ship.shipType.Equals("Patrol Boat"))
+                {
+                   
+
+                    Button toDrawStart = (Button)PlayerShipTable.FindName(ship.getCoords()[0, 0]+ ship.getCoords()[0, 1]);
+                    toDrawStart.Content = "O";
+                    Button toDrawEnd = (Button)PlayerShipTable.FindName(ship.getCoords()[1, 0] + ship.getCoords()[1, 1]);
+                    toDrawEnd.Content = "O";
+                }else if (ship.shipType.Equals("Submarine"))
+                {
+                    player_name_test_label.Content = ship.getCoords()[1, 0];
+                    player_name_test_label.Content = ship.getCoords()[1, 1];
+
+                    Button toDrawStart = (Button)PlayerShipTable.FindName(ship.getCoords()[0, 0] + "" + ship.getCoords()[0, 1]);
+                    toDrawStart.Content = "O";
+                    Button toDrawMid = (Button)PlayerShipTable.FindName(ship.getCoords()[1, 0] + "" + ship.getCoords()[1, 1]);
+                    toDrawMid.Content = "O";
+                    Button toDrawEnd = (Button)PlayerShipTable.FindName(ship.getCoords()[2, 0] + "" + ship.getCoords()[2, 1]);
+                    toDrawEnd.Content = "O";
+                }
+            }
+        }
+
+
+
+
+
     }
 }
