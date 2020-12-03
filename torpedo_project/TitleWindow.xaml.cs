fbb2ed50e45372bca95ce2551df766299dc05609
Balázs Tóth <gawnoris@gmail.com>
2020-12-03 17:22:26 +0100

    edited some events with the title window

--- torpedo_project/TitleWindow.xaml.cs
+++ torpedo_project/TitleWindow.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace torpedo_project
 {
@@ -37,7 +27,7 @@ namespace torpedo_project
             }
         }
 
-        private void player_name_box_KeyDown(object sender, KeyEventArgs e)
+        private void player_name_box_MouseEnter(object sender, MouseEventArgs e)
         {
             player_name_box.Background = Brushes.White;
         }
