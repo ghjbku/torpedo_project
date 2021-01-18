5d615b463c14dc29ca951a6e5f72cc6a36b72977
Balázs Tóth <gawnoris@gmail.com>
2021-01-18 13:36:12 +0100

    adding the aitest's project

---
+++ Torpedo_prj_Test/AiLogicTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Torpedo_prj_Test
+{
+    [TestClass]
+    public class AiLogicTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
